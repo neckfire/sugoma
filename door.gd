ded6069b3017c43be871dcdd1a3d3a9619c17b12
neckfire <mgdboisgrollier69@gmail.com>
2023-06-08 06:58:38 +0000

    added files

--- door.gd
+++ door.gd
@@ -3,7 +3,8 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$MeshInstance3D.visible=true
+	$MeshInstance3D/StaticBody3D.set_collision_layer_value(1, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
